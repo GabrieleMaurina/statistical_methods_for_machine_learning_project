77afcd372e8bbbc7f53aac1079e1a498cba20889
GabrieleMaurina <gabrielemaurina95@gmail.com>
2021-05-13 11:14:54 +0200

    Experiment setup ready.

--- example.py
+++ example.py
@@ -4,6 +4,8 @@ This script tests a basic model.
 '''
 
 import tensorflow as tf
+import tensorflow_docs as tfdocs
+import numpy as np
 from dataset import dataset,labels
 
 size = 30
@@ -26,7 +28,20 @@ model.compile(optimizer='adam',
               metrics=['accuracy'])
 
 print('### training model')
-model.fit(x_train,y_train,epochs=4)
+history = model.fit(x_train,y_train,epochs=4)
+
+plotter = tfdocs.plots.HistoryPlotter(metric='binary_crossentropy',smoothing_std=10)
+plotter.plot(size_histories)
+plt.ylim([0.5, 0.7])
+
+exit(0)
 
 print('### evaluating model')
 print(model.evaluate(x_test,y_test,verbose=2))
+
+print('### zero one loss')
+y_pred = np.argmax(model.predict(x_test),axis=1)
+zero_one_loss =np.sum(y_pred!=y_test)/y_test.shape[0]
+print(zero_one_loss)
+print(y_pred)
+print(y_test)
