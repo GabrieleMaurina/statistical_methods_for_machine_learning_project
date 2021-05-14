b3c966d64f77c58fa532964d69731a3fc26c0892
GabrieleMaurina <gabrielemaurina95@gmail.com>
2021-05-14 07:31:38 +0200

    Experiment 2, first half.

--- experiment_2.py
+++ experiment_2.py
@@ -1,7 +1,11 @@
 #!/usr/bin/env python
 '''
-This is the second experiment. Here all images are used against a single model and the goal is to separate varieties.
+This is the second experiment.
 '''
+
+
+
+
 
 import tensorflow as tf
 import numpy as np
@@ -9,68 +13,86 @@ from os.path import join,isdir,isfile
 from os import mkdir
 from json import dump
 from dataset_2 import dataset
-from models import conv
+from models import models
 
 
 
 
-input_size = 30
-output_size = 132
-depth = 3
-size = 40
-output_file = 'exp2.csv'
-model_name = 'varieties_model'
+output_folder = 'results'
+output_file = join(output_folder,'results.csv')
+input_sizes = [i for i in range(10,51,10)]
+n_labels = 132
 
 
 
 
 def main():
-    print(f'### loading dataset with size {input_size}')
-    x_train,y_train,x_test,y_test = dataset(input_size)
-    x_train = x_train/255
-    x_test = x_test/255
+    '''Second experiment.'''
 
-    #10% for validation
-    val_size = int(round(y_test.shape[0]*0.1))
-    x_val = x_train[:val_size]
-    y_val = y_train[:val_size]
-    x_train = x_train[val_size:]
-    y_train = y_train[val_size:]
-
-    print('### evaluating model')
-
-    model = conv(input_size,output_size,depth,size)
-
-    early_stopping = tf.keras.callbacks.EarlyStopping(monitor='val_loss',patience=2)
-
-    model.compile(optimizer='adam',
-                  loss=tf.keras.losses.SparseCategoricalCrossentropy(from_logits=True),
-                  metrics=['accuracy'])
-
-    history = model.fit(
-        x_train,
-        y_train,
-        epochs=50,
-        validation_data=(x_val,y_val),
-        callbacks=[early_stopping])
-
-    n_epochs = len(history.history['loss'])
-
-    #compute zero-one loss by counting how many predictions
-    #do not match and dividing by number of images
-    y_pred = np.argmax(model.predict(x_test),axis=1)
-    zero_one_loss =np.sum(y_pred!=y_test)/y_test.shape[0]
+    #create output folder if it does not exist
+    if not isdir(output_folder): mkdir(output_folder)
 
     #create csv file if it does not exist
     if not isfile(output_file):
         with open(output_file,'w') as csv:
             csv.write('image_size,model_type,model_depth,model_size,zero_one_loss,n_epochs\n')
 
-    #append data to csv file
-    with open(output_file,'a') as csv:
-        csv.write(f'{input_size},conv,{depth},{size},{zero_one_loss},{n_epochs}\n')
+    #for 5 input sizes
+    for input_size in input_sizes:
+        print(f'### loading dataset with size {input_size}')
+        x_train,y_train,x_test,y_test = dataset(input_size)
+        x_train = x_train/255
+        x_test = x_test/255
 
-    model.save(model_name)
+        #10% for validation
+        val_size = int(round(y_test.shape[0]*0.1))
+        x_val = x_train[:val_size]
+        y_val = y_train[:val_size]
+        x_train = x_train[val_size:]
+        y_train = y_train[val_size:]
+
+        #for 30 models
+        for model,depth,size,type in models(input_size,n_labels):
+            history_file = join(output_folder,f'{input_size}_{type}_{depth}_{size}.json')
+
+            #if it wasn't already computed
+            if not isfile(history_file):
+
+                print(f'### evaluating model {input_size},{type},{depth},{size}')
+
+                early_stopping = tf.keras.callbacks.EarlyStopping(monitor='val_loss',patience=2)
+
+                model.compile(optimizer='adam',
+                              loss=tf.keras.losses.SparseCategoricalCrossentropy(from_logits=True),
+                              metrics=['accuracy'])
+
+                #fitting a total of 150 models
+                #Maximum 50 epochs, but it can stop early based on val_loss
+                history = model.fit(
+                    x_train,
+                    y_train,
+                    epochs=50,
+                    validation_data=(x_val,y_val),
+                    callbacks=[early_stopping])
+
+                n_epochs = len(history.history['loss'])
+
+                #compute zero-one loss by counting how many predictions
+                #do not match and dividing by number of images
+                y_pred = np.argmax(model.predict(x_test),axis=1)
+                zero_one_loss =np.sum(y_pred!=y_test)/y_test.shape[0]
+
+                #append data to csv file
+                with open(output_file,'a') as csv:
+                    csv.write(f'{input_size},{type},{depth},{size},{zero_one_loss},{n_epochs}\n')
+
+                #save training history
+                with open(history_file,'w') as out:
+                    dump(history.history,out,sort_keys=True,indent=4)
+
+                #clear space
+                del model
+                tf.keras.backend.clear_session()
 
 
 
