d4f3de4f085dd37d765073077a1e33fbfc784889
GabrieleMaurina <gabrielemaurina95@gmail.com>
2021-05-14 19:05:33 +0200

    Added some plots and tables.

--- report/build.sh
+++ report/build.sh
@@ -2,6 +2,6 @@
 
 export TEXINPUTS=..//:
 pdflatex --shell-escape report &&
-pdflatex --shell-escape report &&
+pdflatex --shell-escape report
 rm -rf report.aux report.log _minted-report report.toc
 xdg-open report.pdf
