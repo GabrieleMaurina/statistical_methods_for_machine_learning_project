a52f11a557b5b2880a41113c203b94f277e8102c
GabrieleMaurina <gabrielemaurina95@gmail.com>
2021-05-16 10:47:12 +0200

    Improved report.

--- report/build.sh
+++ report/build.sh
@@ -2,7 +2,7 @@
 
 export TEXINPUTS=..//:
 pdflatex --shell-escape report &&
-bibtex report.aux
+bibtex report.aux &&
 pdflatex --shell-escape report &&
 pdflatex --shell-escape report
 rm -rf report.aux report.log _minted-report report.toc report.bbl report.blg report.out
